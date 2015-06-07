6bce27c2fecfcbc463d2a0d3830ad0c5ea5e463d
Ashley M. Clark <amclark42@gmail.com>
2015-06-06 22:01:04 -0400

    Add document edits and new file "partsOfAnXPath"
    Where "xpathBasics" is intended to act as a memory aid or cheat
    sheet, "partsOfAnXPath" has a narrative to it - a (hopefully)
    friendly introduction to the parts that make up an XPath.

--- resources/scripts/xplorator.js
+++ resources/scripts/xplorator.js
@@ -4,10 +4,15 @@ $('#tabbasics a').click(function (e) {
   e.preventDefault()
   $(this).tab('show')
 });
-$('#tabbasics a').click(function (e) {
+$('#tabparts a').click(function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+});
+$('#tabwhy a').click(function (e) {
   e.preventDefault()
   $(this).tab('show')
 });
 
 $('#tabbasics a[href="#basics"]').tab('show')
+$('#tabparts a[href="#parts"]').tab('show')
 $('#tabwhy a[href="#why"]').tab('show')
