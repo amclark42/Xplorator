e0c0dd8908cacdd55df2d301704768b659ac3f1b
Ashley M. Clark <amclark42@gmail.com>
2020-01-18 11:42:20 -0500

    Begin work on JS parser for XPath
    xplorator.js can recognize some simple XPaths: requests for
    children or descendant elements.

--- resources/scripts/xplorator.js
+++ resources/scripts/xplorator.js
@@ -10,19 +10,58 @@ var xplr = xplr || {};
     when the context changes. */
   var that = this;
   
+  
   /*** Private functions ***/
   
-  var getSurrogate = function (htmlid) { };
+  /* Given a string assumed to be XPath, determine the next step of the query. */
+  var identifyStep = function(xpath) {
+    var regexStep, match, ns,
+        stepInfo = {};
+    regexStep =
+      /^\/(?<axis>\/)?\s?(?<gi>(?:(?<prefix>\w+):)?[_\p{Letter}][\w_\.-]*)/u;
+    match = regexStep.exec(xpath);
+    // Recover from a string that doesn't match expectations about XPath.
+    if ( match === null ) {
+      stepInfo['msg'] = {
+          'type': 'error',
+          'say': "Cannot parse string '"+xpath+"' as XPath."
+        };
+    } else {
+      stepInfo['axis'] = match.groups.axis || 'child';
+      if ( stepInfo.axis === '/' ) {
+        stepInfo['axis'] = 'descendant';
+      }
+      stepInfo['gi'] = match.groups.gi;
+    }
+    return stepInfo;
+  }; // identifyStep()
+  
+  /* A Javascript version of XPath's fn:normalize-space(). Whitespace is deleted 
+    from the beginning and end of the given string. Whitespace elsewhere in the 
+    string is reduced to a single space.  */
+  var normalizeSpace = function(str) {
+    var trimmedStr,
+        regexWsBetween = /\s{2,}/g,
+        regexWsEnds = /(^\s+|\s+$)/g;
+    trimmedStr = str.replace(regexWsEnds, '');
+    return trimmedStr.replace(regexWsBetween, ' ');
+  }; // normalizeSpace()
   
   
   /*** Public functions ***/
   
-  this.clearTextChildren = function (htmlid) { };
+  /* Run a given XPath. */
+  this.execute = function(xpath) {
+    var normStr = normalizeSpace(xpath),
+        step1 = identifyStep(normStr);
+    console.log(normStr);
+    return step1;
+  }; // this.execute()
   
   
   /*** Class definitions ***/
   
-  this.MenuOption = class {
+  this.XmlDoc = class {
     constructor () { }
   };
 }).apply(xplr); // Apply the namespace to the anonymous function.
@@ -30,7 +69,8 @@ var xplr = xplr || {};
 
 // Create a callback function to be run when the entire document has loaded.
 var onLoad = function() {
-  console.log("Do something");
+  var test = xplr.execute("/mtx:sc/mtx:ti//mtx:fe");
+  console.log(test);
 };
 
 /* Ensure that the callback function above is run, whether or not the DOM has 
